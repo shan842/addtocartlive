7fea872b2d257882a478b5b48f3f724ff9fe2f75
shan842 <skrjha842@gmail.com>
2021-07-05 19:29:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Cart from "./component/Cart";
+import Cart from "./components/Cart";
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <Cart />
+    </>
+  );
+};
+
+export default App;
